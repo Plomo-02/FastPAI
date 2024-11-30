69d3b014f0fa94bd6139d1bcc256a0516d74f472
Alfredo <alfredotoselli0@gmail.com>
2024-11-30 12:23:34 +0100

    react app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { Alert } from 'design-react-kit';
+import 'bootstrap-italia/dist/css/bootstrap-italia.min.css';
+import 'typeface-titillium-web';
+import 'typeface-roboto-mono';
+import 'typeface-lora';
+
+function App() {
+  return <Alert>This is an Alert</Alert>;
+}
+
+export default App;
